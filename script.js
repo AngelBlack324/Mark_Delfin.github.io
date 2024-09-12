96d5d7072213e4e2a643079169d8e191da282611
AngelBlack324 <166625488+AngelBlack324@users.noreply.github.com>
2024-09-12 22:19:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from submitting the traditional way
+    alert('Thank you for your message! I will get back to you soon.');
+});
